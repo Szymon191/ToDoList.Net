0281690e93f75d864dfbb80dd274302bd2e13834
Szymon Szymanski <82146087+Szymon191@users.noreply.github.com>
2024-07-03 16:03:14 +0200

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using ToDoList.Core;
+using ToDoList.Database;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var database = new ToDoListDbContext();
+
+            database.Database.EnsureCreated(); // upewnia sie czy baza jest stworzona jak nie to ja tworzy na nowo
+
+            DataBaseLocator.Database = database;
+        }
+    }
+
+}
---
+++ Pages/WorkTaskPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using ToDoList.Core;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WorkTaskPage.xaml
+    /// </summary>
+    public partial class WorkTaskPage : Page
+    {
+        public WorkTaskPage()
+        {
+            InitializeComponent();
+
+            DataContext = new WorkTasksPageViewModel();
+        }
+    }
+}
